7e5667899ff32946a4b1dd6b39da66277217127e
OHMURA Tokiya <tocky.dison@gmail.com>
2021-01-27 15:44:00 +0900

    fix: modify local_search function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import numpy as np
 import copy
+import tqdm
 
 
 class Frog:
@@ -28,6 +29,7 @@ class Frog:
         for i in range(self.dimension):
             if np.random.rand() < p_m:
                 self.x[i] = (self.x[i] + 1) % 2
+        self.fitness = self.calc_fitness()
 
 
 class MDSFLA:
@@ -40,19 +42,22 @@ class MDSFLA:
         self.P = 200  # population size
         self.m = 10  # number of memeplexes
         self.iMax = 100  # number of iterations within each memeplex
-        self.p_m = 0  # genetic mutation probability
+        self.p_m = 0.06  # genetic mutation probability
         # frogs
         self.frogs = np.array([])
+        self.best_frog = None
 
     def generate_frogs(self):
         for i in range(self.P):
             # evaluate the fitness of the frog
             self.frogs = np.append(self.frogs, Frog(self.dimension, self.weight, self.value, self.capacity))
+        self.best_frog = self.frogs[0]
 
     def local_search(self, memeplexes):
         x_g = memeplexes[0][0]
         for memeplex in memeplexes:
             for it in range(self.iMax):
+                memeplex = sorted(memeplex, key=lambda x: x.fitness, reverse=True)
                 x_b = memeplex[0]
                 x_w = memeplex[-1]
                 x_w_new = copy.deepcopy(x_w)
@@ -65,6 +70,8 @@ class MDSFLA:
                 x_w_new.fitness = x_w_new.calc_fitness()
                 if x_w_new.fitness > x_w.fitness:
                     memeplex[-1] = x_w_new
+                    if x_w_new.fitness > x_g.fitness:
+                        x_g = x_w_new
                     continue
                 # apply eqn. 2, 3 and 6 with replacing x_b with x_g
                 for i in range(self.dimension):
@@ -73,19 +80,26 @@ class MDSFLA:
                     u = np.random.rand()
                     x_w_new.x[i] = 0 if t <= u else 1
                 x_w_new.fitness = x_w_new.calc_fitness()
+
                 if x_w_new.fitness > x_w.fitness:
                     memeplex[-1] = x_w_new
+                    if x_w_new.fitness > x_g.fitness:
+                        x_g = x_w_new
                     continue
                 x_w_new.x = x_w_new.generate_x()
                 x_w_new.fitness = x_w_new.calc_fitness()
                 memeplex[-1] = x_w_new
+                if x_w_new.fitness > x_g.fitness:
+                    x_g = x_w_new
+        if x_g.fitness > self.best_frog.fitness:
+            self.best_frog = x_g
         return memeplexes
 
     def solve(self):
         # generate population of P frogs randomly
         self.generate_frogs()
 
-        for _ in range(self.iMax):
+        for _ in tqdm.tqdm(range(self.iMax)):
             # sort P frogs in descending order
             self.frogs = sorted(self.frogs, key=lambda x: x.fitness, reverse=True)
             # partition P frogs into m memeplexes
@@ -96,17 +110,20 @@ class MDSFLA:
             # local search
             cur_memeplexes = self.local_search(cur_memeplexes)  # -> shuffled the m memeplexed
             self.frogs = cur_memeplexes.flatten()
+            np.random.shuffle(self.frogs)
             # apply mutation on the population
             for frog in self.frogs:
                 frog.mutation(self.p_m)
+                if frog.fitness > self.best_frog.fitness:
+                    self.best_frog = frog
 
         # determine the best solution
         self.frogs = sorted(self.frogs, key=lambda x: x.fitness, reverse=True)
-        return self.frogs[0]
+        return self.best_frog
 
 
 def main():
-    problem = "p07"
+    problem = "p08"
     # read problem files
     f = open(f"../problem/{problem}/{problem}_c.txt", "r")
     data = f.read()
@@ -124,7 +141,22 @@ def main():
     # solve
     sfla = MDSFLA(capacity, weight, value)
     result = sfla.solve()
-    print(result.x, result.fitness)
+    print(result.x, all(result.x == ans), result.fitness)
+    w = 0
+    v = 0
+    for j in range(len(result.x)):
+        if result.x[j]:
+            w += weight[j]
+            v += value[j]
+    print(w, v)
+    w = 0
+    v = 0
+    for j in range(len(ans)):
+        if ans[j]:
+            w += weight[j]
+            v += value[j]
+    print(w, v)
+
 
 
 if __name__ == "__main__":
