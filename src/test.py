bcd3c345264739d6daffcc61bac0f504b66a03fa
OHMURA Tokiya <tocky.dison@gmail.com>
2021-01-29 05:12:01 +0900

    fix: output log on each time

--- src/test.py
+++ src/test.py
@@ -8,7 +8,7 @@ def main():
     n = 30
     now = int(time.time())
     with open(f"../log/log_{now}.csv", "a") as log:
-        log.write("problem, best, worst, average, std\n")
+        log.write("problem, best, worst, average, std, data\n")
     for i in range(1, 9):
         problem = f"p0{i}"
         # read problem files
@@ -30,13 +30,13 @@ def main():
             result = cur_bfwa.solve()
             result_bfwa.append(result.gorgeous_degree)
         with open(f"../log/log_{now}.csv", "a") as log:
-            log.write(f"{problem}, {max(result_bfwa)}, {min(result_bfwa)}, {mean(result_bfwa)}, {stdev(result_bfwa)}\n")
+            log.write(f"{problem}, {max(result_bfwa)}, {min(result_bfwa)}, {mean(result_bfwa)}, {stdev(result_bfwa)}, {', '.join(map(str, result_bfwa))}\n")
         for _ in range(n):
             cur_sfla = MDSFLA(capacity, weight, value)
             result = cur_sfla.solve()
             result_sfla.append(result.fitness)
         with open(f"../log/log_{now}.csv", "a") as log:
-            log.write(f"{problem}, {max(result_sfla)}, {min(result_sfla)}, {mean(result_sfla)}, {stdev(result_sfla)}\n")
+            log.write(f"{problem}, {max(result_sfla)}, {min(result_sfla)}, {mean(result_sfla)}, {stdev(result_sfla)}, {', '.join(map(str, result_sfla))}\n")
 
 
 if __name__ == "__main__":
